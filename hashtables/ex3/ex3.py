3737bf1bb8baab0b531606c38a86bb3072f63c05
Khalil <khalilfoulks@gmail.com>
2020-10-02 12:28:32 -0400

    finished ex 3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,6 +4,30 @@ def intersection(arrays):
     """
     # Your code here
 
+    # create a cache
+    cache = {}
+    # create a answers array
+    result = []
+
+    # loop through both arrays  
+    for array in arrays:
+        for num in array:
+            # if num not in cache add each item in 2nd array as key
+            if num not in cache:
+                # set value to 1
+                cache[num] = 1
+            # if key exists
+            else:
+                # each time a key appears in another array increment by 1
+                cache[num] += 1
+                # if count matches # of arrays num is in every array
+                if cache[num] == len(arrays):
+                    # add it to reults array
+                    result.append(num)
+
+    # print(cache)
+
+
     return result
 
 
